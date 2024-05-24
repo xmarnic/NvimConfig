07e4a9e58929488ea18a2ab81bef7d9785082d1d
Nicholas Marlin <nickxmar@gmail.com>
2024-05-24 16:43:07 -0600

    [feat] restore cursor position when opening a file
    Implemented an autocommand to retore the cursor position
    when opening a file.

--- lua/melkey/autocmds.lua
+++ lua/melkey/autocmds.lua
@@ -17,3 +17,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 	desc = "Highlight words when a yank (y) is performed",
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		local last_pos = vim.fn.line("'\"")
+		if last_pos > 0 and last_pos <= vim.fn.line("$") then
+			vim.api.nvim_win_set_cursor(0, { last_pos, 0 })
+		end
+	end,
+})
