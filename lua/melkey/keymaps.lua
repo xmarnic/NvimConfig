0528a58316cb0b8d6da6d5798015e47f484ec2f2
Nicholas Marlin <nickxmar@gmail.com>
2024-05-28 09:52:09 -0600

    feat: define keymaps for quick save and exit

--- lua/melkey/keymaps.lua
+++ lua/melkey/keymaps.lua
@@ -3,6 +3,14 @@ vim.g.mapleader = " "
 -- Copy to clipboard
 vim.keymap.set("v", "<C-c>", [["+y]])
 
+-- Escape insert mode
+vim.keymap.set("i", "jk", "<Esc>")
+
+-- Quick save and exit
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>wq", ":wq<CR>")
+
 -- Movement
 vim.keymap.set("n", "<leader>h", "<Cmd>wincmd h<CR>", { desc = "Move cursor to left window" })
 vim.keymap.set("n", "<leader>j", "<Cmd>wincmd j<CR>", { desc = "Move cursor to bottomw window" })
@@ -13,9 +21,9 @@ vim.keymap.set("n", "<leader>-", "<Cmd>vertical resize -5<CR>", { desc = "Resize
 vim.keymap.set("n", "<C-S>", "<Cmd>write<CR>", { desc = "Save buffer" })
 
 -- Tabs
-vim.keymap.set("n", "<leader>q", "<Cmd>bprevious<CR>", { desc = "Go to previous buffer" })
+--vim.keymap.set("n", "<leader>q", "<Cmd>bprevious<CR>", { desc = "Go to previous buffer" })
 vim.keymap.set("n", "<leader>e", "<Cmd>bnext<CR>", { desc = "Go to next buffer" })
-vim.keymap.set("n", "<leader>w", "<Cmd>bdelete<CR>", { desc = "Delete buffer" })
+--vim.keymap.set("n", "<leader>w", "<Cmd>bdelete<CR>", { desc = "Delete buffer" })
 
 -- Diagnostics
 -- utils.key_mapper('n', '<leader>dn', ':lua vim.diagnostic.goto_next({float={border="rounded"}})<CR>')
