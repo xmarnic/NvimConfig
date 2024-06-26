a50295660fad4ebf1fda8bf565e0b53e1c9aa367
Nicholas Marlin <nickxmar@gmail.com>
2024-06-26 16:02:23 -0600

    🔧 modify cmp keymap

--- lua/xmarnic/plugins/lsp.lua
+++ lua/xmarnic/plugins/lsp.lua
@@ -318,7 +318,7 @@ return {
 				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 				["<C-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 				["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-				["C-e"] = cmp.mapping({
+				["<leader>e"] = cmp.mapping({
 					i = cmp.mapping.abort(),
 					c = cmp.mapping.close(),
 				}),
